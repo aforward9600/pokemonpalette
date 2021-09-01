9363b64e4242b2ed79772f684ab3cdd48316a180
jojobear13 <joseph.billo@gmail.com>
2021-08-31 22:38:25 -0500

    AI switch scoring now penalizes bad match-ups between player and enemy 'mon types

--- custom_functions/func_ai.asm
+++ custom_functions/func_ai.asm
@@ -328,7 +328,7 @@ ScoreAIParty:
 	jr nz, .next0	;go to next check if hp > 0
 	ld a, $0F
 	ld [de], a
-	jp .next6
+	jp .end_scoring_modules
 .next0	
 	
 	;+2 score if faster than current player mon's speed
@@ -357,20 +357,20 @@ ScoreAIParty:
 	call nc, .plus
 	pop af
 	jr nc, .next2
-	;-2 score if less than 3/4 base hp
+	;-1 score if less than 3/4 base hp
 	ld a, $34
+	call AIRosterScoringCheckIfHPBelowFraction
+	ld b, 1
+	call c, .minus
+	;-2 more (total of -3) score if less than 1/2 base hp
+	ld a, 2
 	call AIRosterScoringCheckIfHPBelowFraction
 	ld b, 2
 	call c, .minus
-	;-3 more (total of -5) score if less than 1/2 base hp
-	ld a, 2
-	call AIRosterScoringCheckIfHPBelowFraction
-	ld b, 3
-	call c, .minus
-	;-5 more (total of -10) score if less than 1/4 base hp
+	;-2 more (total of -5) score if less than 1/4 base hp
 	ld a, 4
 	call AIRosterScoringCheckIfHPBelowFraction
-	ld b, 5
+	ld b, 2
 	call c, .minus
 .next2	
 
@@ -460,36 +460,7 @@ ScoreAIParty:
 	cp $02	;regular damaging moves have power > 1
 	jr c, .playermoveloop	;skip out if the move is not a normal damaging move
 	;get effectiveness of the most recent player move
-	ld a, [wUnusedC000]
-	set 3, a 
-	ld [wUnusedC000], a
-	;preserve the current enemy mon typing
-	ld a, [wEnemyMonType]
-	ld [wAIPartyMonScores + 6], a
-	ld a, [wEnemyMonType + 1]
-	ld [wAIPartyMonScores + 7], a
-	;override the current enemy mon typing with that from the roster pointer
-	push bc
-	ld bc, $05
-	call GetRosterStructData
-	ld [wEnemyMonType], a
-	ld bc, $06
-	call GetRosterStructData
-	ld [wEnemyMonType + 1], a
-	pop bc
-	;now get the typing effectiveness
-	push bc
-	push hl
-	push de
-	callab AIGetTypeEffectiveness
-	pop de
-	pop hl
-	pop bc
-	;now undo the current mon type override
-	ld a, [wAIPartyMonScores + 6]
-	ld [wEnemyMonType], a
-	ld a, [wAIPartyMonScores + 7]
-	ld [wEnemyMonType + 1], a
+	call .get_effectiveness_to_enemy
 	;now get the move-to-type effectiveness
 	ld a, [wTypeEffectiveness]
 	;skip if effectiveness is neutral
@@ -519,9 +490,8 @@ ScoreAIParty:
 	;at this point the move must be super effective
 	;minus based on the power of the move
 	ld a, [wPlayerMovePower]
-	srl a
-	srl a
-	srl a
+	srl a	;-1/2 power
+	srl a	;-1/4 power
 	ld b, a
 	call .minus	
 	jp .playermoveloop
@@ -622,6 +592,44 @@ ScoreAIParty:
 .next7	
 
 
+	;score penalty if opponent could have STAB against the pointed-to enemy mon
+	ld a, [wPlayerMoveType]
+	push af
+	;check player type 1 effectiveness
+	ld a, [wBattleMonType]
+	ld [wPlayerMoveType], a
+	call .get_effectiveness_to_enemy
+	ld b, 3	;-3 for 2x effective or -6 if 4x effective
+	ld a, [wTypeEffectiveness]
+	push af
+	cp $10
+	call nc, .minus
+	pop af
+	cp $15
+	call nc, .minus
+	;jump if there is no type 2
+	push hl
+	ld hl, wBattleMonType
+	ld a, [wBattleMonType + 1]
+	cp [hl]
+	pop hl
+	jr z, .next8
+	;else do type 2 now
+	ld [wPlayerMoveType], a
+	call .get_effectiveness_to_enemy
+	ld b, 3	;-3 for 2x effective or -6 if 4x effective
+	ld a, [wTypeEffectiveness]
+	push af
+	cp $10
+	call nc, .minus
+	pop af
+	cp $15
+	call nc, .minus
+.next8	
+	pop af
+	ld [wPlayerMoveType], a
+	
+.end_scoring_modules
 	pop bc
 	dec b
 	jr z, .donescoring
@@ -639,14 +647,53 @@ ScoreAIParty:
 .plus
 	ld a, [de]
 	add b
+	call c, .overflow
 	ld [de], a
 	ret
 .minus
 	ld a, [de]
 	sub b
+	call c, .underflow
 	ld [de], a
 	ret
-
+.overflow
+	ld a, $FF
+	ret
+.underflow
+	ld a, $00
+	ret
+.get_effectiveness_to_enemy
+	ld a, [wUnusedC000]
+	set 3, a 
+	ld [wUnusedC000], a
+	;preserve the current enemy mon typing
+	ld a, [wEnemyMonType]
+	ld [wAIPartyMonScores + 6], a
+	ld a, [wEnemyMonType + 1]
+	ld [wAIPartyMonScores + 7], a
+	;override the current enemy mon typing with that from the roster pointer
+	push bc
+	ld bc, $05
+	call GetRosterStructData
+	ld [wEnemyMonType], a
+	ld bc, $06
+	call GetRosterStructData
+	ld [wEnemyMonType + 1], a
+	pop bc
+	;now get the typing effectiveness
+	push bc
+	push hl
+	push de
+	callab AIGetTypeEffectiveness
+	pop de
+	pop hl
+	pop bc
+	;now undo the current mon type override
+	ld a, [wAIPartyMonScores + 6]
+	ld [wEnemyMonType], a
+	ld a, [wAIPartyMonScores + 7]
+	ld [wEnemyMonType + 1], a
+	ret
 	
 	
 
