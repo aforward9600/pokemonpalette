5386f86129da42f0a79270b9668498a3df93e56b
jojobear13 <joseph.billo@gmail.com>
2021-12-03 00:29:53 -0600

    adjust saffron guard text for accuracy

--- text/maps/saffron_gates.asm
+++ text/maps/saffron_gates.asm
@@ -3,9 +3,10 @@ _SaffronGateText_1dfe7::
 	line "Gee, I'm thirsty,"
 	cont "though!"
 
+;joenote - add a hint on what to give the guard
 	para "I wish that I had"
-	line "some FRESHWATER"
-	cont "right now."
+	line "a VENDING MACHINE"
+	cont "drink right now."
 
 	para "Oh wait there,"
 	line "the road's closed."
