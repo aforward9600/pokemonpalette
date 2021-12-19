c1e2ddaf803f40de62c0613b39327bb750002ac7
jojobear13 <joseph.billo@gmail.com>
2021-12-19 00:53:11 -0600

    fixing text delay flags getting cleared and adding hram reference material

--- hram.asm
+++ hram.asm
@@ -310,8 +310,10 @@ hCoordsInFrontOfPlayerMatch EQU $FFEA
 
 hSpriteAnimFrameCounter EQU $FFEA
 
+H_WHOFIRST EQU $FFF2 ; joenote - 0 on player going first, 1 on enemy going first
 H_WHOSETURN EQU $FFF3 ; 0 on player’s turn, 1 on enemy’s turn
-H_WHOFIRST EQU $FFF4 ; joenote - 0 on player going first, 1 on enemy going first
+
+hClearLetterPrintingDelayFlags EQU $FFF4
 
 ; bit 0: draw HP fraction to the right of bar instead of below (for party menu)
 ; bit 1: menu is double spaced
